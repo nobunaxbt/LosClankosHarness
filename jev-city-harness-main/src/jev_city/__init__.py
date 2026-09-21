cddc6638e888365fedbe258f351312e7d3008a23
nobu <lestercrestvirtual@gmail.com>
2026-09-21 14:25:24 +0200

    Delete jev-city-harness-main directory

--- jev-city-harness-main/src/jev_city/__init__.py
+++
@@ -1,3 +0,0 @@
-"""A local, single-character controller powered by Jev decisions."""
-
-__version__ = "0.1.0"
--- jev-city-harness-main/src/jev_city/errors.py
+++
@@ -1,2 +0,0 @@
-class DecisionInterrupted(Exception):
-    """A new command or threat invalidated an in-flight decision."""
